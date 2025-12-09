b7a7cdaa90ce74cf2a7afa4429fbd3bf57c12657
Babak <32584918+BabakRosewater@users.noreply.github.com>
2025-12-09 14:52:11 -0800

    Create files_map.js

---
+++ files_map.js
@@ -0,0 +1,9 @@
+const FILES = {
+  assets: "assets.csv",
+  liabilities_equity: "liabilities_equity.csv",
+  sales_cost_of_sales: "sales_cost_of_sales.csv",
+  additions_deductions: "additions_deductions.csv",
+  expenses: "expenses.csv",
+  guidelines: "guidelines.csv",
+  master: "coa_master.csv",
+};
